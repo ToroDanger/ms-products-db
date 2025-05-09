3f0bd1195351161e3a02f7e4387ba721389383f7
eritodv <diazerick.v@gmail.com>
2025-05-09 01:26:21 -0400

    crod

--- src/main/java/cl/duoc/ms_products_db/controller/ProductController.java
+++ src/main/java/cl/duoc/ms_products_db/controller/ProductController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +32,31 @@ public class ProductController {
         Product savedProduct = productService.createProduct(newProduct);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedProduct);
     }
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
+        boolean deleted = productService.deleteProduct(id);
+        if (deleted) {
+            return ResponseEntity.status(HttpStatus.OK).body("Producto eliminado correctamente.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ID no válido. Producto no encontrado.");
+        }
+    }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable Long id, @RequestBody Product updatedProduct) {
+        boolean updated = productService.updateProduct(id, updatedProduct);
+
+        if (updated) {
+            return ResponseEntity.status(HttpStatus.OK).body("Producto actualizado correctamente.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ID no válido. Producto no encontrado.");
+        }
+    }
+
+
     
+
 
     
     
--- src/main/java/cl/duoc/ms_products_db/service/ProductService.java
+++ src/main/java/cl/duoc/ms_products_db/service/ProductService.java
@@ -1,6 +1,7 @@
 package cl.duoc.ms_products_db.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,36 @@ public class ProductService {
     public Product createProduct(Product product) {
         return productRepository.save(product);
     }
+    //delete
+    public boolean deleteProduct(Long id) {
+        Optional<Product> product = productRepository.findById(id);
+        if (product.isPresent()) {
+            productRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    //put
+    public boolean updateProduct(Long id, Product updatedProduct) {
+    Optional<Product> existingProductOpt = productRepository.findById(id);
+
+    if (existingProductOpt.isPresent()) {
+        Product existingProduct = existingProductOpt.get();
+        
+        // Actualizar los campos del producto
+        existingProduct.setName_product(updatedProduct.getName_product());
+        existingProduct.setQuantity(updatedProduct.getQuantity());
+        existingProduct.setPrice(updatedProduct.getPrice());
+        existingProduct.setDescription(updatedProduct.getDescription());
+        
+        // Guardar el producto actualizado en la base de datos
+        productRepository.save(existingProduct);
+        return true;
+    } else {
+        return false;
+    }
+}
+
     
 }
